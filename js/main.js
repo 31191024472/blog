5cacb717bb2a605e1668205844a2ce1886f3fde3
31191024472 <thanhbe.31191024472@st.ueh.edu.vn>
2023-12-04 22:15:46 +0700

    git

--- js/main.js
+++ js/main.js
@@ -78,3 +78,29 @@ document.addEventListener('click', function(e){
 
 })
 
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Ngăn chặn việc submit form mặc định
+
+        const ten = document.getElementById("ten").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const sdt = document.getElementById("sdt").value.trim();
+
+        // Kiểm tra nếu Họ tên, email và số điện thoại không được nhập
+        if (ten === "" || email === "" || sdt === "") {
+            alert("Vui lòng nhập đầy đủ thông tin");
+            return;
+        }
+
+        // Kiểm tra số điện thoại chỉ chứa ký tự số và không chứa các ký tự khác
+        if (!/^\d+$/.test(sdt)) {
+            alert("Số điện thoại không hợp lệ");
+            return;
+        }
+
+        // Nếu thông tin hợp lệ, tiến hành submit form
+        this.submit();
+    });
+});
